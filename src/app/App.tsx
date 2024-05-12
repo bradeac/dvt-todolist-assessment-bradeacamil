751a77cff9574f6b8bcd7669548c8b602af7cc75
bradeac <bradeacamil@gmail.com>
2024-05-12 14:50:14 +0300

    Extract code to AddTodoForm component.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,49 +1,18 @@
-import { ChangeEvent, FormEvent, useState } from "react";
 import { useSelector } from "react-redux";
-import { v4 as uuid } from "uuid";
 
 import { TodoList } from "../components/TodoList";
-import { add } from "../features/todos/todoListSlice";
-import { RootState, useAppDispatch } from "./store";
+import { RootState } from "./store";
 
 import "./App.css";
+import { AddTodoForm } from "../components/AddTodoForm";
 
 function App() {
-  const dispatch = useAppDispatch();
   const { todos } = useSelector((state: RootState) => state.todos);
-  const [inputValue, setInputValue] = useState<string>("");
-
-  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const value = event.currentTarget.value;
-    setInputValue(value);
-  };
-
-  const handleAdd = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-
-    if (!inputValue) return;
-
-    dispatch(add({ completed: false, id: uuid(), value: inputValue }));
-    setInputValue("");
-  };
 
   return (
     <main>
       <h1 className="mb-16">TODO List</h1>
-      <form className="mb-16" onSubmit={handleAdd}>
-        <section className="flex items-center gap-4">
-          <label htmlFor="todotextinput">I need to do:</label>
-          <input
-            id="todotextinput"
-            name="TODO text input"
-            value={inputValue}
-            onChange={handleInputChange}
-          />
-          <button disabled={!inputValue} type="submit">
-            Add
-          </button>
-        </section>
-      </form>
+      <AddTodoForm />
       <TodoList todos={todos} />
     </main>
   );
