08ddb1676ba624b7a15b6e3ce1dc82d8ad273a42
bradeac <bradeacamil@gmail.com>
2024-05-12 14:57:42 +0300

    Reorder imports.

--- src/features/todos/todoListSlice.ts
+++ src/features/todos/todoListSlice.ts
@@ -1,4 +1,5 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
 import { Todo } from "../../types/Todo.type";
 
 export interface TodosState {
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { store } from "./app/store.ts";
 
 import App from "./app/App.tsx";
+import { store } from "./app/store.ts";
+
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
